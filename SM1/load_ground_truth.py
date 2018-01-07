c50c46971c73301f370de3f9df0a919add1a4531
Robert Fischer <robert@programming-with-design.at>
2018-01-07 14:54:20 +0100

    tried to apply solution of sm1 to sm2, but does not work

--- SM1/load_ground_truth.py
+++ SM1/load_ground_truth.py
@@ -7,16 +7,20 @@ def load_data(file):
 
     def convert_time(time):
         if isinstance(time, str):
-            minute = int(time[0:2])
-            second = int(time[3:5])
+            time_comps = time.split(':')
+
+            minute = int(time_comps[0])
+            second = int(time_comps[1])
             return minute*60 + second
         else:
             return -1
 
     j1 = df["Jump #1"].apply(convert_time)
     j2 = df["Jump #2"].apply(convert_time)
+    w1 = df["Winch #1"].apply(convert_time)
+    w2 = df["Winch #2"].apply(convert_time)
 
-    df = pd.DataFrame({"name": df["Filename"], "j1": j1, "j2": j2}, columns=("name", "j1", "j2"))
+    df = pd.DataFrame({"name": df["Filename"], "j1": j1, "j2": j2, "w1": w1, "w2": w2}, columns=("name", "j1", "j2", "w1", "w2"))
 
     return df
 
